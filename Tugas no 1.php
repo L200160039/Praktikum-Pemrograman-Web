61cba5a819be7a1138e198dd7c57a096092e87f7
L200160039 <43208692+L200160039@users.noreply.github.com>
2018-09-24 08:26:21 +0700

    Add files via upload

---
+++ Tugas no 1.php
@@ -0,0 +1,12 @@
+<html>
+	<head>
+		<title>Background Gambar</title>
+	</head>
+	<body background='L200160039.jpg'>
+		<h1>Biodata Diri:</h1>
+		<h2>Nama : Nofri Novita Sari<br>
+		NIM : L200160039<br>
+		Kota : Wonogiri</h2>
+	</body>
+</html>
+	
